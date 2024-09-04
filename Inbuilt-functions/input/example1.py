e0b1f013e7938746c302517ae527dab987f505e6
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-09-04 05:39:23 +0000

    input function examples

---
+++ Inbuilt-functions/input/example1.py
@@ -0,0 +1,12 @@
+"""
+Input function is a pre-define in-built function.
+it is used as assining values to variables directly from the keyboard.
+example:
+"""
+a=input("enter a value for a:")#by default the value stores in string
+print("the value is:",a)
+print(type(a))#type() function is used as to know the variable is in which data-type.
+b=input("enter your name:")
+print("your name is:",b)
+print(type(b))
+
---
+++ Inbuilt-functions/input/numeric-datatype.py
@@ -0,0 +1,10 @@
+a=int(input("enter the first value:"))#here we are directly assining the value to the variable in int type.
+print("the value of a is:",a)
+print(type(a))
+b=float(input("enter the second value is:"))#here we are assining the value to the variable in float type.
+print("the value of b is:",b)
+print(type(b))
+c=input("enter the third number:")
+print("the value of c is :",c)
+print(type(c))
+
